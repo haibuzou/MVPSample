df52989ae0ac096d2a5b4660f6e61c4112fd07a8
haibuzou <haibuzouxi@163.com>
2016-03-16 18:09:05 +0800

    Initial commit

---
+++ app/src/main/java/haibuzou/mvpsample/mvp/MVPActivity.java
@@ -0,0 +1,15 @@
+package haibuzou.mvpsample.mvp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import haibuzou.mvpsample.R;
+
+public class MVPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mvp);
+    }
+}
